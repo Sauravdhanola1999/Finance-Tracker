6541953f47aefa538872971b19bdd3acab8cdea9
Saurav Singh Dhanola <sauravsingh16101999@gmail.com>
2025-04-20 14:27:50 +0530

    folder structure

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/page.tsx
-export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to Your Personal Finance Visualizer</h1>
-      <p>Start tracking your expenses and budgets today!</p>
-    </div>
-  );
-}
